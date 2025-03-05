ab77eac439fc7ec90d54f2c251174f42fd215177
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-03-05 12:37:22 -0500

    Adds in functionality to finish iteration 2

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Market do
         it 'exists' do
             market = Market.new("South Pearl Street Farmers Market")
 
-            expect(market)to be_a(Market)
+            expect(market).to be_a(Market)
         end
 
         it 'has attributes' do
