682d47f57442c766b5e2651abcf564d68ef56bb8
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-03-05 11:12:14 -0500

    Initial spec file setup and first test

---
+++ spec/item_spec.rb
@@ -0,0 +1,22 @@
+require './lib/item'
+
+RSpec.describe Item do
+    describe '#initialize' do
+        it 'exists' do
+            item1 = Item.new({name: 'Peach', price: "$0.75"})
+
+            expect(item1).to be_a(Item)
+        end
+
+        it 'has attributes' do
+            item1 = Item.new({name: 'Peach', price: "$0.75"})
+            item2 = Item.new({name: 'Tomato', price: '$0.50'})
+
+            expect(item1.name).to eq('Peach')
+            expect(item1.price).to eq(0.75)
+
+            expect(item1.name).to eq('Tomato')
+            expect(item2.price).to eq(0.5)
+        end
+    end
+end
